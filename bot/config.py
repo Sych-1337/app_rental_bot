5f25046874da53033d82c95e193d92f89ca549cf
Sych-1337 <53253377+Sych-1337@users.noreply.github.com>
2025-06-02 19:43:36 +0300

    Мои локальные изменения

--- bot/config.py
+++ bot/config.py
@@ -1,7 +1,10 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+# Попробуем загрузить .env только если он реально существует (чтобы не мешать Render)
+env_path = os.path.join(os.path.dirname(__file__), '.env')
+if os.path.exists(env_path):
+    load_dotenv(dotenv_path=env_path)
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 DATABASE_URL = os.getenv("DATABASE_URL")
